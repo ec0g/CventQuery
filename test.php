3350ec69d286f0e9b45276918f6ba07e77236caf
Goce Evtimovski <goce@goce.co>
2015-11-03 11:30:39 -0500

    start the login class testing

--- test.php
+++ test.php
@@ -4,6 +4,10 @@ require_once 'vendor/autoload.php';
 
 use CventApi\CventLogin;
 use CventApi\CventLoginCredentials;
+use Dotenv\Dotenv;
 
+$env = new Dotenv(__DIR__);
+$env->load();
 
+///$login = new CventLogin(getenv('WSDL'))->
 
